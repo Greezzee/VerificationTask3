b7d5fafa2527eba7f9d0e261f6393467a3aa6904
Zorin Eugene <39644926+Greezzee@users.noreply.github.com>
2024-05-21 07:26:10 +0300

    initiation of refunds

--- src/shop.cpp
+++ src/shop.cpp
@@ -31,9 +31,6 @@ bookshop::OrderID bookshop::Bookshop::makeDeliveryOrder(Cart cart, DeliveryMetho
     for (auto& bookOrder : cart.getAllOrderedBooks()) {
         if (m_booksByID.find(bookOrder.id) == m_booksByID.end())
             continue;
-        
-        if (m_books.find(m_booksByID[bookOrder.id]) == m_books.end())
-            continue;
 
         auto& foundBook = m_books[m_booksByID[bookOrder.id]];
 
@@ -109,4 +106,38 @@ std::vector<bookshop::BooksPack> bookshop::Bookshop::transferToBuyer(OrderID id)
     }
 
     return out;
+}
+
+void bookshop::Bookshop::initiateRefund(OrderID id, RefundType refundType, std::string from_addr) {
+    if (m_orders.find(id) == m_orders.end())
+        return;
+    
+    auto& order = m_orders[id];
+
+    if (refundType == RefundType::UNDELIVERED_ORDER_REFUND && 
+       (order.status == OrderStatus::ACCEPTED ||
+        order.status == OrderStatus::AWAITING_PICKUP ||
+        order.status == OrderStatus::IN_DELIVERY)) {
+
+        order.refundType = RefundType::UNDELIVERED_ORDER_REFUND;
+        order.status = OrderStatus::REFUND_INITIATED;   
+    }
+    else if (refundType == RefundType::SELF_REFUND && order.status == OrderStatus::FINISHED) {
+        order.refundType = RefundType::SELF_REFUND;
+        order.status = OrderStatus::REFUND_INITIATED;   
+    }
+    else if (refundType == RefundType::COURIER_REFUND && order.status == OrderStatus::FINISHED && !from_addr.empty()) {
+        order.refundType = RefundType::COURIER_REFUND;
+        order.status = OrderStatus::REFUND_INITIATED;  
+        order.deliveryAddress = from_addr;
+    }
+
+}
+
+void bookshop::Bookshop::deliverRefund(OrderID id) {
+
+}
+
+void bookshop::Bookshop::finishRefund(OrderID id) {
+
 }
